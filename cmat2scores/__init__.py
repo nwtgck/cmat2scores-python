82602e35f990656910ddf6b6af3e9cdadef69f32
Ryo Ota <nwtgck@gmail.com>
2018-01-05 02:39:31 +0900

    [add] Create first installable pip project (not impl yet)

---
+++ cmat2scores/__init__.py
@@ -0,0 +1,2 @@
+def hello_str():
+  return "hello, world"
---
+++ tests/cmat2scores_test.py
@@ -0,0 +1,12 @@
+import unittest
+from sklearn import metrics
+
+# TODO: impl
+class Cmat2scoresTest(unittest.TestCase):
+    assert(True)
+
+
+def suite():
+  suite = unittest.TestSuite()
+  suite.addTest(unittest.makeSuite(Cmat2scoresTest))
+  return suite
