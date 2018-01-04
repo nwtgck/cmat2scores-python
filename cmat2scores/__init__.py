f3289895566e993e371048bc3db8911168802413
Ryo Ota <nwtgck@gmail.com>
2018-01-05 02:53:16 +0900

    [add] Implement cmat_to_psuedo_y_true_and_y_pred() function

--- cmat2scores/__init__.py
+++ cmat2scores/__init__.py
@@ -1,2 +1,13 @@
-def hello_str():
-  return "hello, world"
+def cmat_to_psuedo_y_true_and_y_pred(cmat):
+  """
+  Convert a confusion matrix to psuedo y_true and y_pred
+  :param cmat: Confusion matrix
+  :return: psuedo y_true and y_pred
+  """
+  y_true = []
+  y_pred = []
+  for true_class, row in enumerate(cmat):
+    for pred_class, elm in enumerate(row):
+      y_true.extend([true_class] * elm)
+      y_pred.extend([pred_class] * elm)
+  return y_true, y_pred
--- tests/cmat2scores_test.py
+++ tests/cmat2scores_test.py
@@ -1,9 +1,20 @@
 import unittest
 from sklearn import metrics
+import numpy as np
+import cmat2scores
 
-# TODO: impl
 class Cmat2scoresTest(unittest.TestCase):
-    assert(True)
+  cmat = np.array([
+    [3, 2, 0],
+    [2, 1, 0],
+    [0, 0, 3]
+  ])
+  # Create y_true and y_pred
+  psuedo_y_true, psuedo_y_pred = cmat2scores.cmat_to_psuedo_y_true_and_y_pred(cmat)
+  # Calc cmat by sklearn
+  sklearn_cmat = metrics.confusion_matrix(psuedo_y_true, psuedo_y_pred)
+  # Should be equal
+  assert(np.array_equal(cmat, sklearn_cmat))
 
 
 def suite():
